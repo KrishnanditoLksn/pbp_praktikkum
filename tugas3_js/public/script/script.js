de60f908be2a6a83737275ae27f8ffcccdab2043
Gorthdzx <krislaksana123@gmail.com>
2024-02-28 09:19:41 +0700

    feat : added  ok button listener

--- tugas3_js/public/script/script.js
+++ tugas3_js/public/script/script.js
@@ -1,11 +1,15 @@
 const submitButton = document.querySelector('#submit')
-const capres = ['Anis', 'Ganjar']
+const parentButton = document.getElementById('buttonParent')
+const text = document.createTextNode("OK")
+const username = document.getElementById('inputUsername').value
 
 submitButton.addEventListener('click', (ev) => {
     ev.preventDefault()
     const amountChoice = document.getElementById('jumlahPilihan').value
     const list = document.getElementById('choiceList')
     const choiceId = document.getElementById('choiceId')
+    const button = document.createElement('button')
+    button.className = "btn btn-danger"
     let temp = " "
     let tempList = " "
     if (amountChoice.length <= 0) {
@@ -14,9 +18,16 @@ submitButton.addEventListener('click', (ev) => {
         for (let i = 1; i <= amountChoice; i++) {
             temp += `Pilihan ${i}: <br>`
             tempList += ` Pilihan ${i} <br>`
+            button.appendChild(text)
+            parentButton.appendChild(button)
         }
     }
     list.innerHTML = tempList
     choiceId.innerHTML = temp
-})
 
+
+    button.addEventListener('click', (ev) => {
+        ev.preventDefault()
+        alert("Ok")
+    })
+})
